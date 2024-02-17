9395b2812be24cf304dae77b65e72383c2f3c7a5
Shravan Kumar <shraankumar87922@gmail.com>
2024-02-17 22:24:11 +0530

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import './App.css';
+import SearchComponent from './SearchComponent';
+import ShowCourseComponent from './ShowCourseComponent';
+import UserCartComponent from './UserCartComponent';
+ 
+function App() {
+    const [courses, setCourses] = useState([
+        { id: 1, 
+          name: 'T-shirt', 
+          price: 499, 
+          image:'https://pbx2-pbww-prod-pbww-cdn.getprintbox.com/media/productimage/fe050866-8169-4645-ad76-10437425291c/Blank%20T-Shirt_thumb_300x300?mt=1588770269.372995'
+        },
+        { id: 2, 
+          name: 'College Bag', 
+          price: 699, 
+          image:'https://smartsalez.in/EaseSalezDoc//ProductMainImage/roshan-AMERICAN-TOURISTER-28L-SEST-02-BACKPACK-RED.jpg'
+        },
+        { id: 3, 
+          name: 'Hoodie', 
+          price: 799, 
+          image:'https://images.meesho.com/images/products/55009214/1htfq_512.webp'
+        }
+    ]);
+ 
+    const [cartCourses, setCartCourses] = useState([]);
+    const [searchCourse, setSearchCourse] = useState('');
+ 
+    const addCourseToCartFunction = (GFGcourse) => {
+        const alreadyCourses = cartCourses
+                               .find(item => item.product.id === GFGcourse.id);
+        if (alreadyCourses) {
+            const latestCartUpdate = cartCourses.map(item =>
+                item.product.id === GFGcourse.id ? { 
+                ...item, quantity: item.quantity + 1 } 
+                : item
+            );
+            setCartCourses(latestCartUpdate);
+        } else {
+            setCartCourses([...cartCourses, {product: GFGcourse, quantity: 1}]);
+        }
+    };
+ 
+    const deleteCourseFromCartFunction = (GFGCourse) => {
+        const updatedCart = cartCourses
+                            .filter(item => item.product.id !== GFGCourse.id);
+        setCartCourses(updatedCart);
+    };
+ 
+    const totalAmountCalculationFunction = () => {
+        return cartCourses
+               .reduce((total, item) => 
+                           total + item.product.price * item.quantity, 0);
+    };
+ 
+    const courseSearchUserFunction = (event) => {
+        setSearchCourse(event.target.value);
+    };
+ 
+    const filterCourseFunction = courses.filter((course) =>
+        course.name.toLowerCase().includes(searchCourse.toLowerCase())
+    );
+ 
+    return (
+        <div className="App">
+            <SearchComponent searchCourse={searchCourse} 
+                             courseSearchUserFunction=
+                                 {courseSearchUserFunction} />
+            <main className="App-main">
+                <ShowCourseComponent
+                    courses={courses}
+                    filterCourseFunction={filterCourseFunction}
+                    addCourseToCartFunction={addCourseToCartFunction}
+                />
+ 
+                <UserCartComponent
+                    cartCourses={cartCourses}
+                    deleteCourseFromCartFunction={deleteCourseFromCartFunction}
+                    totalAmountCalculationFunction={
+                        totalAmountCalculationFunction
+                    }
+                    setCartCourses={setCartCourses}
+                />
+            </main>
+        </div>
+    );
+}
+ 
+export default App;
